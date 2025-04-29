b4674987466e85bab768e292b455158355fa8e1e
Randi K <randiquek@gmail.com>
2025-04-28 22:41:41 -0400

    updated database with monster/item inserts

--- dungeoncrawler-db.sql
+++ dungeoncrawler-db.sql
@@ -24,18 +24,19 @@ create table item (
 	item_id int primary key auto_increment,
     item_name varchar(128) not null, 
     item_type varchar(50) not null,
-    effect_value int not null
+    effect_value int not null,
+    description TEXT not null
 );    
 
 create table inventory (
 	inventory_id int primary key auto_increment,
 	player_id char(36) not null,
-	item_name varchar(128) not null,
+	item_id int not null,
 	quantity int not null,
 		foreign key (player_id)
         references player (player_id),
-        foreign key (item_name)
-        references item (item_name)
+        foreign key (item_id)
+        references item (item_id)
 );
 
 create table battle (
@@ -49,3 +50,20 @@ create table battle (
         foreign key (monster_id)
         references monster (monster_id)
 );
+
+insert into monster (monster_name, level, monster_hp, attack, xp_reward) values
+('Slime', 1, 20, 3, 10),
+('Goblin', 2, 35, 5, 20),
+('Skeleton', 3, 50, 7, 30),
+('Wolf', 4, 60, 8, 40),
+('Orc', 5, 80, 10, 50),
+('Dark Mage', 6, 70, 12, 60),
+('Troll', 7, 120, 15, 80),
+('Dragonling', 8, 150, 18, 100);
+
+insert into item (item_name, item_type, effect_value, description) values
+('Health Potion', 'potion', 25, 'Restores 25 HP'),
+('Damage Potion', 'potion', 10, 'Temporarily increases attack by 10'),
+('Iron Sword', 'weapon', 5, 'A basic sword that adds 5 damage'),
+('Apprentice Wand', 'weapon', 4, 'A simple wand that adds 4 magic damage'),
+('Short Bow', 'weapon', 6, 'A lightweight bow that adds 6 ranged damage');
